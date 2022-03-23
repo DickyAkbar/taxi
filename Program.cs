22940da576fe6d078ddfa421df86e192ab80d040
DickyAkbarBaihaqki <dickyakbarb@students.amikom.ac.id>
2022-03-23 20:16:43 +0700

    program Taxi

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugastaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi taxi1 = new taxi();
+
+            taxi1.drivername = "Jono";
+            taxi1.onduty = true;
+            taxi1.numpassenger = 10;
+
+            taxi1.taxiinfo();
+            taxi1.pickuppassenger();
+            taxi1.dropoffpassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugastaxi
+{
+    public class taxi
+    {
+        public string drivername { get; set; }
+        public bool onduty { get; set; }
+        public int numpassenger { get; set; }
+
+        private string yesno()
+        {
+            return onduty ? "Yes" : "No";
+        }
+
+        public void taxiinfo()
+        {
+            Console.WriteLine("Driver Name : {0}", drivername);
+            Console.WriteLine("On Duty : {0} ", yesno());
+            Console.WriteLine("Number of Passenger : {0}", numpassenger);
+        }
+
+        public void pickuppassenger()
+        {
+            Console.WriteLine(Environment.NewLine+"{0} Sedang Menjemput penumpang", drivername);
+        }
+
+        public void dropoffpassenger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", drivername);
+        }
+    }
+}
